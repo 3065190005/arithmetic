2f01d5a76d22fd99825df01d15c7301605a58709
DevilFish <3065190005@qq.com>
2021-07-19 20:17:11 +0800

    chagne the file name

---
+++ Script/GameManager.ts
@@ -0,0 +1,35 @@
+export namespace Global {
+	export class GameRule{
+		private constructor(){
+			this.initOnFile();
+		}
+
+		private static m_Instance : GameRule = null!;
+
+		public static getInstance(): GameRule{
+			if(!this.m_Instance){
+				this.m_Instance = new GameRule();
+			}
+			return this.m_Instance;
+		}
+
+		public initOnFile():void{
+
+		}
+
+		set setPlayerName(_name:string)
+			{this.m_playerName = _name;}
+
+		get getPlayerName():string
+			{return this.m_playerName;}
+
+		set setPlayerLevel(_level:number)
+			{this.m_playerLevel = _level;}
+
+		get getPlayerLevel():number
+			{return this.m_playerLevel;}
+		
+		private m_playerName:string;
+		private m_playerLevel:number;
+	};
+};
