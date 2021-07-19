e5322d5ff935f836d783bd1aa9804dc4a9eef650
DevilFish <3065190005@qq.com>
2021-07-19 20:19:48 +0800

    write Idle Function and get some Component

--- Script/character.ts
+++ Script/character.ts
@@ -1,12 +1,25 @@
 const {ccclass, property} = cc._decorator;
 
+import dF = require("./TypeDefine"); 
+import gM = require("./GameManager");
+
 @ccclass
 export class Character extends cc.Component {
 	
 	onLoad():void{
 		this.m_animeB = "null";
 		this.m_animeA = "Idle";
-		
+
+		cc.Node bodyBox = this.parent.bodyBox.node;
+		cc.Node AttBox = this.parent.attackBox.node;
+		this.m_BodyBox = bodyBox?.getComponents(cc.BoxCollider);
+		this.m_AttBox = AttBox?.getComponents(cc.BoxCollider);
+
+		this.m_animation = cc.getComponent(cc.Animation);
+
+		this.m_status = dF.CUSDefine.State.None;
+
+		this.onIdle();
 	}
 
 	update(dt:number):void{
@@ -32,42 +45,72 @@ export class Character extends cc.Component {
 	@property({displayName:"CharaterLevel"})
 	m_baseLevel:number = 1;
 
+	// 当前和上一个动画暂存
 	m_animeA:string;
 	m_animeB:string;
 
-	public onIdle(){
+	// 当前状态
+	m_status:dF.CUSDefine.State;
 
+	// 动画组件
+	m_animation:cc.Animation;
+
+	// 碰撞体组件
+	m_BodyBox:cc.BoxCollider[];
+	m_AttBox:cc.BoxCollider[];
+
+	// 行为树
+	public StateTree():void{
+		
 	}
 
-	public onMove(){
-
+	public onIdle():boolean{
+		if(!this.onChangeStatus("Idle"))
+			return false;
+		
+		this.m_animation.play("Idle");
 	}
 
-	public onJump(){
+	public onMove():boolean{
 
+		return false;
 	}
 
-	public onAttack(){
+	public onJump():boolean{
 
+		return false;
+	}
+
+	public onAttack():boolean{
+
+		return false;
 	}	
 
-	public onHurt(){
+	public onHurt():boolean{
+
+		return false;
+	}
+
+	public onDead():boolean{
+		return false;
+	}
+
+	private onChangeStatus(action:string):boolean{
+		if(this.m_animeA == this.m_animeB){
+			return false;
+		}
+
+		this.m_animeB = this.m_animeA;
+		this.m_animeA = action;
+		this.m_status = dF.CUSDefine.StatusVec[this.m_animeA];
+		return true;
+	}
+
+	public AttBoxUpdate(id:number,x:number,y:number,w:number,h:number):void{
 
 	}
 
-	public onDead(){
-
-	}
-
-	private onChangeStatus(){
-
-	}
-
-	public AttBoxUpdate(id:number,x:number,y:number,w:number,h:number){
-
-	}
-
-	public ClyBoxUpdate(id:number,x:number,y:number,w:number,h:number){
+	public ClyBoxUpdate(id:number,x:number,y:number,w:number,h:number):void{
 		
 	}
 };
